45d6b8485149d8197cd336936428f22081c6727d
ArdiRizqiansyah <ardirizqiansyah@gmail.com>
2023-07-09 08:37:20 +0800

    make main header navigation with link

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { Route, Routes } from "react-router-dom";
+import Welcome from "./pages/Welcome";
+import Products from "./pages/Products";
+import MainHeader from "./components/MainHeader";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <MainHeader />
+      <main>
+        <Routes>
+          <Route exact path="/welcome" element={<Welcome />} />
+          <Route path="/products" element={<Products />} />
+        </Routes>
+      </main>
+    </div>
+  );
 }
 
 export default App;
